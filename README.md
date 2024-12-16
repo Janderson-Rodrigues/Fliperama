0467a2e0d0485967469c126181cfd0d0eb84d2af
Janderson <jandersonrodrigues216@gmail.com>
2024-12-16 13:34:12 -0300

    commit for page to game

---
+++ README.md
@@ -0,0 +1,38 @@
+# Jogo do click fantasma. 👻👻⛓️‍💥⛓️‍💥
+
+## Turma: FNS1
+Curso de desenvolvimento Full stack
+Módulo Intermediário.
+**Professores**: Fernanda Nascimento, Gabriel Bezerra.
+
+**Equipe: 02**
+
+- Anildo da Silva Gomes.
+- Anderson Oliveira de Sousa.
+- Ivan Magalhães.
+- Janderson Carneiro Rodrigues
+
+---
+
+### Apresentação do projeto
+
+_Objetivo_
+
+- Aplicar conceitos Javascript visto em aula para criar um jogo interativo.
+> function
+
+> const
+
+> mousemove
+
+- Criar um jogo onde um botão muda de posição aleatoriamente quando o mouse chega perto dele.
+Descrição
+
+- Um botão aparece no centro da tela com o texto "Clique-me se puder!".
+Quando o usuário move o mouse próximo ao botão (usando mousemove), ele se reposiciona aleatoriamente dentro da área da página.
+O desafio termina quando o usuário consegue clicar no botão.
+Conceitos Envolvidos
+
+- Eventos de mouse (mousemove, click).
+Manipulação de estilos (style.top, style.left).
+Cálculo de posições aleatórias.
---
+++ script.js
@@ -0,0 +1,46 @@
+alert("ATENÇÃO, MINISTÉRIO DOS GAMES ADVERTE, ESTE JOGO PODE CAUSAR EXTRESSE🤬🤬🤬😡")
+document.addEventListener('DOMContentLoaded', () => { 
+    const button = document.getElementById('ta-amarrado');
+    const imagem = document.getElementById('fundo');
+
+    button.addEventListener('mousemove', () => { 
+        moveButtonRandomly(button);
+    });
+    
+    button.addEventListener('click', () => { 
+        imagem.setAttribute("src", "MIDIAS/sim.jpg");
+    });
+
+    falaTexto("h1", "Quer casar comigo?");
+
+    setTimeout(() => {
+        responsiveVoice.speak("Pq você está demorando para responder? Você não me ama mais?", 'Brazilian Portuguese Female', {rate:0.9});
+    }, 8000);
+
+    setTimeout(() => {
+        imagem.setAttribute("src", "MIDIAS/eleaceita.jpg");
+        responsiveVoice.speak("Gente ele está muito emocionado, eu vou responder por ele. ele aceita", 'Brazilian Portuguese Female', {rate:1.0});
+    }, 20000);
+    
+});
+
+function falaTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.0});
+}
+
+function moveButtonRandomly(button) { 
+    const gameArea = button.parentElement; 
+    const areaWidth = gameArea.clientWidth; 
+    const areaHeight = gameArea.clientHeight; 
+    const buttonWidth = button.offsetWidth; 
+    const buttonHeight = button.offsetHeight; 
+    
+    const randomX = Math.random() * (areaWidth - buttonWidth); 
+    const randomY = Math.random() * (areaHeight - buttonHeight); 
+    
+    button.style.left = `${randomX}px`; 
+    button.style.top = `${randomY}px`; 
+}
+
